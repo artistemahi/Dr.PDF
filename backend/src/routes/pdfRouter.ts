a3981c34b077f8d08f6ec2bd07c2e34ca0d92a27
Mahesh kumar <vickymandal542@gmail.com>
2026-05-20 22:18:34 +0530

    pdf/merge api done

--- backend/src/routes/pdfRouter.ts
+++ backend/src/routes/pdfRouter.ts
@@ -1,28 +1,90 @@
+import fs from "fs";
 import express from "express";
+import { OptionalAuth } from "../middleware/auth";
+import { upload } from "../config/multer";
+import mergePdf from "../utils/mergepdf";
+
 const pdfRouter = express.Router();
-import {OptionalAuth} from "../middleware/auth";
 
 // pdf/merge
-pdfRouter.post("/pdf/merge", OptionalAuth,async (req, res)=>{
+pdfRouter.post(
+  "/pdf/merge",
+  OptionalAuth,
+  upload.array("files", 10),
+  async (req, res) => {
+    try {
+      const files = req.files as Express.Multer.File[];
+      if (!files || files.length == 0) {
+        return res.status(400).json({
+          success: false,
+          message: "No file uploaded !",
+        });
+      }
+      const filePaths: any = files.map((file: any) => {
+        return file.path;
+      });
+      const mergedPdf: any = await mergePdf(filePaths);
+      filePaths.forEach((path: any) => {
+        if (fs.existsSync(path)) {
+          fs.unlinkSync(path);
+        }
+      });
+      res.setHeader("Content-Type", "application/pdf");
+      res.setHeader(
+        "Content-Disposition",
+        `attachment; filename=merged-${Date.now()}.pdf`,
+      );
 
-});
+      res.send(Buffer.from(mergedPdf));
+    } catch (err: any) {
+      res.status(500).json({
+        success: false,
+        message: err.message,
+      });
+    }
+  },
+);
 
 // pdf/split
-pdfRouter.post("/pdf/split", OptionalAuth,async (req, res)=>{
-
+pdfRouter.post("/pdf/split", OptionalAuth, async (req, res) => {
+  try {
+  } catch (err: any) {
+    res.status(400).json({
+      success: false,
+      message: err.message,
+    });
+  }
 });
 
 // pdf/extract-pages
-pdfRouter.post("/pdf/extract-pages", OptionalAuth,async (req, res)=>{
-
+pdfRouter.post("/pdf/extract-pages", OptionalAuth, async (req, res) => {
+  try {
+  } catch (err: any) {
+    res.status(400).json({
+      success: false,
+      message: err.message,
+    });
+  }
 });
 
 // pdf/delete-pages
-pdfRouter.post("/pdf/delete-pages", OptionalAuth,async (req, res)=>{
-
+pdfRouter.post("/pdf/delete-pages", OptionalAuth, async (req, res) => {
+  try {
+  } catch (err: any) {
+    res.status(400).json({
+      success: false,
+      message: err.message,
+    });
+  }
 });
 
 // pdf/reorder-pages
-pdfRouter.post("/pdf/reorder-pages", OptionalAuth,async (req, res)=>{
-
+pdfRouter.post("/pdf/reorder-pages", OptionalAuth, async (req, res) => {
+  try {
+  } catch (err: any) {
+    res.status(400).json({
+      success: false,
+      message: err.message,
+    });
+  }
 });
---
+++ backend/src/utils/mergepdf.ts
@@ -0,0 +1,22 @@
+import {PDFDocument} from 'pdf-lib'
+import fs from "fs"
+const mergePdf = async (filePaths:String)=>{
+ const emptypdf =await PDFDocument.create();
+
+ for(const filePath of filePaths){
+    // reading th file    
+    const PdfByte = fs.readFileSync(filePath);
+    // loading the existing pdf 
+    const pdf =await PDFDocument.load(PdfByte);
+    const pages= await emptypdf.copyPages(
+        pdf,
+        pdf.getPageIndices()
+    );
+
+    pages.forEach((page)=>{
+       return  emptypdf.addPage(page);
+    });
+    const mergedPdfBytes = await emptypdf.save();
+ }
+}
+export default mergePdf;
