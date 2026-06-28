57237938a0ab369fae5e7d11a1fb823dc2e6151d
Mahesh kumar <vickymandal542@gmail.com>
2026-06-29 00:59:09 +0530

    word-to-pdf api done

--- backend/src/routes/conversionRouter.ts
+++ backend/src/routes/conversionRouter.ts
@@ -1,9 +1,10 @@
-import express,{ Request, Response } from "express";
+import express, { Request, Response } from "express";
 import { upload } from "../config/multer";
 import { OptionalAuth } from "../middleware/auth";
-import {pdfToWord} from "./../services/conversion/PdfToWord"
-import fs from "fs"
-export  const conversionRouter = express.Router();
+import { pdfToWord } from "./../services/conversion/pdfToWord";
+import { wordToPdf } from "./../services/conversion/wordToPdf";
+import fs from "fs";
+export const conversionRouter = express.Router();
 
 // convert/pdf-to-word
 conversionRouter.post(
@@ -11,58 +12,100 @@ conversionRouter.post(
   OptionalAuth,
   upload.single("file"),
   async (req: Request, res: Response) => {
-    let uploadedFilePath = ""
-    let outputFilePath =""
-    try{
-      const file = req.file as Express.Multer.File
-      if(!file){
+    let uploadedFilePath = "";
+    let outputFilePath = "";
+    try {
+      const file = req.file as Express.Multer.File;
+      if (!file) {
         throw new Error("No file uploaded");
       }
-      if(file.mimetype!=="application/pdf"){
+      if (file.mimetype !== "application/pdf") {
         throw new Error("Only pdf are allowed");
       }
-      uploadedFilePath =file.path;
-      outputFilePath= `uploads/temp/converted-${Date.now()}.docx`
+      uploadedFilePath = file.path;
+      outputFilePath = `uploads/temp/converted-${Date.now()}.docx`;
       // convert
-      const result = await pdfToWord(uploadedFilePath,outputFilePath)
-      res.download(result,(err)=>{
-        if(outputFilePath && fs.existsSync(outputFilePath)){
-          fs.unlinkSync(outputFilePath)
+      const result = await pdfToWord(uploadedFilePath, outputFilePath);
+      res.download(result, (err) => {
+        if (outputFilePath && fs.existsSync(outputFilePath)) {
+          fs.unlinkSync(outputFilePath);
         }
-      })
-    }catch(err:any){
-     return res.status(400).json({
-        success:false,
-        message:err.message
-      })
-    }finally{
-        if(uploadedFilePath && fs.existsSync(uploadedFilePath)){
-          fs.unlinkSync(uploadedFilePath)
-        }
+      });
+    } catch (err: any) {
+      return res.status(400).json({
+        success: false,
+        message: err.message,
+      });
+    } finally {
+      if (uploadedFilePath && fs.existsSync(uploadedFilePath)) {
+        fs.unlinkSync(uploadedFilePath);
+      }
     }
   },
 );
-// convert/pdf-to-word
+// convert/word-to-pdf
+conversionRouter.post(
+  "/converting/word-to-pdf",
+  OptionalAuth,
+  upload.single("file"),
+  async (req: Request, res: Response) => {
+    let uploadedFilePath = "";
+    let outputFilePath = "";
+    try {
+      const file = req.file as Express.Multer.File;
+      if (!file) {
+        throw new Error("No file uploaded");
+      }
+      const allowedMimeTypes:string[] = [
+        "application/msword", // .doc
+        "application/vnd.openxmlformats-officedocument.wordprocessingml.document", // .docx
+        "application/vnd.oasis.opendocument.text", // .odt
+        "application/rtf", // .rtf
+      ];
+      if(!allowedMimeTypes.includes(file.mimetype)){
+          throw new Error("Only word files (.doc, .docx, .odt, .rtf) are allowed");
+      }
+       
+      uploadedFilePath = file.path;
+      outputFilePath = `uploads/temp`;
+      // convert
+      const result = await wordToPdf(uploadedFilePath, outputFilePath);
+      res.download(result, (err) => {
+        if (result && fs.existsSync(result)) {
+          fs.unlinkSync(result);
+        }
+      });
+    } catch (err: any) {
+      return res.status(400).json({
+        success: false,
+        message: err.message,
+      });
+    } finally {
+      if (uploadedFilePath && fs.existsSync(uploadedFilePath)) {
+        fs.unlinkSync(uploadedFilePath);
+      }
+    }
+  },
+);
+// convert/pdf-to-excel
 conversionRouter.post(
   "/convert/pdf-to-excel",
   OptionalAuth,
   upload.single("file"),
   async (req: Request, res: Response) => {
-    let uploadedFilePath = ""
-    try{
-      const file = req.file as Express.Multer.File
-      if(!file){
+    let uploadedFilePath = "";
+    try {
+      const file = req.file as Express.Multer.File;
+      if (!file) {
         throw new Error("No file uploaded");
       }
-      if(file.mimetype!=="application/pdf"){
+      if (file.mimetype !== "application/pdf") {
         throw new Error("Only pdf are allowed");
       }
-      uploadedFilePath =file.path;
-      // convert 
-    }catch{
-
-    }finally{
-
+      uploadedFilePath = file.path;
+      // convert
+    } catch {
+    } finally {
     }
   },
 );
@@ -72,21 +115,19 @@ conversionRouter.post(
   OptionalAuth,
   upload.single("file"),
   async (req: Request, res: Response) => {
-    let uploadedFilePath = ""
-    try{
-      const file = req.file as Express.Multer.File
-      if(!file){
+    let uploadedFilePath = "";
+    try {
+      const file = req.file as Express.Multer.File;
+      if (!file) {
         throw new Error("No file uploaded");
       }
-      if(file.mimetype!=="application/pdf"){
+      if (file.mimetype !== "application/pdf") {
         throw new Error("Only pdf are allowed");
       }
-      uploadedFilePath =file.path;
-      // convert 
-    }catch{
-
-    }finally{
-
+      uploadedFilePath = file.path;
+      // convert
+    } catch {
+    } finally {
     }
   },
 );
@@ -96,21 +137,19 @@ conversionRouter.post(
   OptionalAuth,
   upload.single("file"),
   async (req: Request, res: Response) => {
-    let uploadedFilePath = ""
-    try{
-      const file = req.file as Express.Multer.File
-      if(!file){
+    let uploadedFilePath = "";
+    try {
+      const file = req.file as Express.Multer.File;
+      if (!file) {
         throw new Error("No file uploaded");
       }
-      if(file.mimetype!=="application/pdf"){
+      if (file.mimetype !== "application/pdf") {
         throw new Error("Only pdf are allowed");
       }
-      uploadedFilePath =file.path;
-      // convert 
-    }catch{
-
-    }finally{
-
+      uploadedFilePath = file.path;
+      // convert
+    } catch {
+    } finally {
     }
   },
 );
@@ -120,22 +159,19 @@ conversionRouter.post(
   OptionalAuth,
   upload.single("file"),
   async (req: Request, res: Response) => {
-    let uploadedFilePath = ""
-    try{
-      const file = req.file as Express.Multer.File
-      if(!file){
+    let uploadedFilePath = "";
+    try {
+      const file = req.file as Express.Multer.File;
+      if (!file) {
         throw new Error("No file uploaded");
       }
-      if(file.mimetype!=="application/pdf"){
+      if (file.mimetype !== "application/pdf") {
         throw new Error("Only pdf are allowed");
       }
-      uploadedFilePath =file.path;
-      // convert 
-    }catch{
-
-    }finally{
-
+      uploadedFilePath = file.path;
+      // convert
+    } catch {
+    } finally {
     }
   },
 );
-
